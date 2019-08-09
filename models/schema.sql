e7812ac365f7810f2ba2b6cb4a9151ae3d36dddf
Michael Morris <mmorris1990@gmail.com>
2019-08-08 20:15:07 -0500

    added heroku functionality for db

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,28 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS class_db;
+CREATE DATABASE class_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE class_db;
+
+CREATE TABLE calendar (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    month VARCHAR(255) NOT NULL,
+    day VARCHAR(255) NOT NULL,
+    event_id INTEGER,
+    event_name VARCHAR(255),
+    event_description TEXT
+);
+
+CREATE TABLE student_calendar (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    day_id INTEGER NOT NULL,
+    month VARCHAR(255) NOT NULL,
+    day VARCHAR(255) NOT NULL,
+    event_name VARCHAR(255),
+    event_description TEXT
+);
+
+CREATE TABLE users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+)
