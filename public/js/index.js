0f017e4aa02294670c16f235ba57d81e7b858e14
Tez, Gardner <tezgardner1212@gmail.com>
2019-08-09 11:45:14 -0500

    added apis for front end ajax calls

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,77 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var data = {
+  // Get references to page elements
+  description: $("#example-text"),
+  month: $("#example-description"),
+  date: $("#submit"),
+  day: $("#example-list")
 
+}
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
+  addEvent: function (data, personsType) {
+
+    if (personsType == "teacher") {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/teach",
+        data: JSON.stringify(data),
+        success: location.reload()
+      });
+    } else if (personsType == "student") {
+
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/student",
+        data: JSON.stringify(example),
+        success: location.reload()
+      });
+    }
   },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
+  getInfo: function (personsType) {
+    if (personsType == "student") {
+      return $.ajax({
+        url: "api/student",
+        type: "GET"
+      });
+    }
+    else if (personsType == "teacher") {
+      return $.ajax({
+        url: "api/teacher",
+        type: "GET"
+      });
+    };
   },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
+  deleteEvent: function (id, personsType) {
+    if (personsType == "student") {
+      return $.ajax({
+        url: "api/student",
+        type: "DELETE"
+      });
+    }
+    else if (personsType == "teacher") {
+      return $.ajax({
+        url: "api/teacher",
+        type: "DELETE"
+      });
+    }
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+// Add event listeners for buttons
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+$addNewEventBtn.on("click", function () {
+  API.addEvent(data, personsType)
+});
 
-      $li.append($button);
+$getData.on("click", function () {
+  API.getInfo(personsType)
+});
 
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$deleteEvent.on("click", ".delete", handleDeleteBtnClick);
