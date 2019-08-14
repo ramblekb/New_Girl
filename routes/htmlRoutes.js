7a0b4d1af469966bcec7476583097d4c5a9d1358
Kip Bradley Smith <ramblekb@gmail.com>
2019-08-13 19:56:01 -0500

    kips changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,75 @@
 var db = require("../models");
+var path = require("path")
 
-module.exports = function(app) {
+module.exports = function (app, passport) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.sendFile(path.join(__dirname, "../public/html/login.html"))
     });
   });
 
+  app.get("/signup", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.sendFile(path.join(__dirname, "../public/html/signup.html"))
+    });
+  });
+
+  // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+      successRedirect: '/', // redirect to the secure profile section
+      failureRedirect: '/signup', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    }));
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/calendar', // redirect to the secure profile section
+    failureRedirect: '/', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }),
+    function (req, res) {
+      console.log("hello");
+
+      if (req.body.remember) {
+        req.session.cookie.maxAge = 1000 * 60 * 3;
+      } else {
+        req.session.cookie.expires = false;
+      }
+      res.redirect('/calendar');
+    });
+
+    app.get('/calendar', isLoggedIn, function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/html/calendar.html"))
+    });
+  
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+    });
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+
 };
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
