0683ea3e8802b991b7fca83aab55b15cf5c871df
Tez, Gardner <tezgardner1212@gmail.com>
2019-08-14 21:01:13 -0500

    updated ajax calls and html routes

--- models/classEvent.js
+++ models/classEvent.js
@@ -1,10 +1,10 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
     var ClassEvent = sequelize.define("Class_Event", {
-        start_date: DataTypes.DATE,
-        end_date: DataTypes.DATE,
-        text: DataTypes.STRING
-
-
+        UserID: DataTypes.INTEGER,
+        Time: DataTypes.DATE,
+        Duration: DataTypes.INTEGER,
+        EventName: DataTypes.STRING,
+        Description: DataTypes.TEXT
     });
     //   var ClassEvent = sequelize.define("Class_Event", {
     //     UserID: DataTypes.INTEGER,
@@ -15,4 +15,15 @@ module.exports = function (sequelize, DataTypes) {
     //     Description: DataTypes.TEXT
     //   });
     return ClassEvent;
-}
+};
+
+  // var checkTime = moment();
+
+  // event.Date = "2:00 pm"
+  // event.Duration = 90
+
+  // checkTime = "3:00 pm"
+
+  // if(checkTime.isAfter(moment(event.Date)) && checkTime.isBefore(moment(event.Date).add(event.Durations, 'm'))){
+  //     console.log("checktime occures during perviously scheduled event")
+  // }
