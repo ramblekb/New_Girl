61240bfee5f6c187d2b72186bc12cf3256c3a314
Michael Morris <mmorris1990@gmail.com>
2019-08-16 20:49:18 -0500

    So many things

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 
   // Find all Records by Artist and return them to the user with res.json
   app.get("/api/records/artist/:artist", function (req, res) {
-    db.sequelize.query("SELECT Username, Artist, Album, Year, WillingToTrade FROM Records r inner join Users u on u.id = r.UserId WHERE r.Artist='" +req.params.artist +"'", { type: db.Sequelize.QueryTypes.SELECT })
+    db.sequelize.query("SELECT Username, Artist, Album, Year, WillingToTrade FROM Records r inner join Users u on u.id = r.UserId WHERE r.Artist='" + req.params.artist + "'", { type: db.Sequelize.QueryTypes.SELECT })
       .then(function (dbRecord) {
         res.json(dbRecord);
       });
@@ -18,7 +18,7 @@ module.exports = function (app) {
 
   // Find all Records by Album and return them to the user with res.json
   app.get("/api/records/album/:album", function (req, res) {
-    db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Records r inner join Users u on u.id = r.UserId Where r.Album = '" + req.params.album +"'", { type: db.Sequelize.QueryTypes.SELECT }).
+    db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Records r inner join Users u on u.id = r.UserId Where r.Album = '" + req.params.album + "'", { type: db.Sequelize.QueryTypes.SELECT }).
       then(function (dbRecord) {
         res.json(dbRecord);
       });
@@ -40,14 +40,6 @@ module.exports = function (app) {
       });
   });
 
-    // Get all record information by Current User
-    app.get("/api/User/id", function (req, res) {
-      db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Users u inner join Records r on u.id = r.UserId Where u.id=" + req.user.id, { type: db.Sequelize.QueryTypes.SELECT }).
-        then(function (data) {
-          res.json(data);
-        });
-    });
-
   // Get Albums that are willing to trade with userId
   app.get("/api/WillingToTrade", function (req, res) {
     db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Records r inner join Users u on u.id = r.UserId Where WillingToTrade=1", { type: db.Sequelize.QueryTypes.SELECT }).
@@ -59,19 +51,26 @@ module.exports = function (app) {
   // Post Record to Record collection
   app.post("/api/addRecord", function (req, res) {
     console.log(req.body)
-    db.Record.create(req.body).then(function (dbRecord) {
+    db.Record.create({
+      Artist: req.body.Artist,
+      Album: req.body.Album,
+      Year: req.body.Year,
+      WillingToTrade: req.body.WillingToTrade,
+      UserId: req.user.id
+    })
+      .then(function (data) {
+        res.json(data);
+      });
+  });
+
+  //Delete Album
+  app.delete("/api/records/delete/:id", function (req, res) {
+    db.Record.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbRecord) {
       res.json(dbRecord);
     });
   });
-  
-    //Delete Album
-    app.delete("/api/records/delete/:id", function (req, res) {
-      db.Record.destroy({
-        where: {
-          id: req.params.id
-        }
-      }).then(function (dbRecord) {
-        res.json(dbRecord);
-      });
-    });
-  };
+};
