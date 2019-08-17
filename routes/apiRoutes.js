20c97693eaefed812e9c84377c4852e068f8ea7e
Rachel Givens <rachelgivens@me.com>
2019-08-17 00:31:59 -0500

    tired

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 
   // Find all Records by Artist and return them to the user with res.json
   app.get("/api/records/artist/:artist", function (req, res) {
-    db.sequelize.query("SELECT Username, Artist, Album, Year, WillingToTrade FROM Records r inner join Users u on u.id = r.UserId WHERE r.Artist='" +req.params.artist +"'", { type: db.Sequelize.QueryTypes.SELECT })
+    db.sequelize.query("SELECT Username, Artist, Album, Year, WillingToTrade FROM Records r inner join Users u on u.id = r.UserId WHERE r.Artist='" + req.params.artist + "'", { type: db.Sequelize.QueryTypes.SELECT })
       .then(function (dbRecord) {
         res.json(dbRecord);
       });
@@ -18,7 +18,7 @@ module.exports = function (app) {
 
   // Find all Records by Album and return them to the user with res.json
   app.get("/api/records/album/:album", function (req, res) {
-    db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Records r inner join Users u on u.id = r.UserId Where r.Album = '" + req.params.album +"'", { type: db.Sequelize.QueryTypes.SELECT }).
+    db.sequelize.query("select Username, Artist, Album, Year, WillingToTrade from Records r inner join Users u on u.id = r.UserId Where r.Album = '" + req.params.album + "'", { type: db.Sequelize.QueryTypes.SELECT }).
       then(function (dbRecord) {
         res.json(dbRecord);
       });
@@ -51,19 +51,26 @@ module.exports = function (app) {
   // Post Record to Record collection
   app.post("/api/addRecord", function (req, res) {
     console.log(req.body)
-    db.Record.create(req.body).then(function (dbRecord) {
+    db.Record.create({
+      Artist: req.body.Artist,
+      Album: req.body.Album,
+      Year: req.body.Year,
+      WillingToTrade: req.body.WillingToTrade,
+      UserId: req.user.id
+    })
+      // .then(function (data) {
+      //   res.json(data);
+      // });
+  });
+
+  //Delete Album
+  app.delete("/api/records/delete/:id", function (req, res) {
+    db.Record.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbRecord) {
       res.json(dbRecord);
     });
   });
-  
-    //Delete Album
-    app.delete("/api/records/delete/:id", function (req, res) {
-      db.Record.destroy({
-        where: {
-          id: req.params.id
-        }
-      }).then(function (dbRecord) {
-        res.json(dbRecord);
-      });
-    });
-  };
+};
